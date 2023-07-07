813e07c9ae99d2c91c12fbe5f5e89f15bcdb68d6
Ryan Andersen <redninja106@live.com>
2023-07-07 14:54:19 -0400

    gameScene stuff

---
+++ GMTK23/GameScene.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMTK23;
+internal class GameScene
+{
+    private readonly List<IGameComponent> components = new();
+    private readonly Queue<IGameComponent> componentsToAdd = new();
+    private readonly Queue<IGameComponent> componentsToRemove = new();
+
+    public void AddComponent(IGameComponent component)
+    {
+        componentsToAdd.Enqueue(component);
+    }
+
+    public void RemoveComponent(IGameComponent component)
+    {
+        componentsToAdd.Enqueue(component);
+    }
+
+    public virtual void Update()
+    {
+        while (componentsToAdd.Count > 0)
+        {
+            components.Add(componentsToAdd.Dequeue());
+        }
+
+        components.ForEach(c => c.Update());
+
+        while (componentsToRemove.Count > 0)
+        {
+            components.Add(componentsToRemove.Dequeue());
+        }
+    }
+
+    public virtual void Render()
+    {
+        components.ForEach(c => c.Render());
+    }
+}
---
+++ GMTK23/IGameComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMTK23;
+internal interface IGameComponent
+{
+    void Render();
+    void Update();
+}
