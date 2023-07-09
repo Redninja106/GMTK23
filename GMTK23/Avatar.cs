d87ac6ec18e51f58c88694e699353a918248e771
Cristiano Porretta <shashaman26@icloud.com>
2023-07-09 00:56:49 -0400

    added-avatar-state

--- GMTK23/Avatar.cs
+++ GMTK23/Avatar.cs
@@ -15,12 +15,30 @@ internal class Avatar : IGameComponent, ISaveable, IFallable
     public Vector2 TargetPos { get; set; }
     public float WalkSpeed { get; set; } = 5;
     public ITexture sprite;
+    public bool hasTorch = false;
+    public AvatarState AvatarState { get; private set; }
+    public bool IsAtTarget => this.Transform.Position == TargetPos;
+    private int currentStateIndex = -1;
+    private AvatarState[] states;
 
+    public void NextState()
+    {
+        currentStateIndex++;
+        AvatarState = states[currentStateIndex];
+    }
     public Avatar(Transform transform)
     {
         Transform = transform;
-        TargetPos = new Vector2(97, Transform.Position.Y);
+        TargetPos = transform.Position;
         sprite = Graphics.LoadTexture("./Assets/dude.png");
+        states = new AvatarState[]
+        {
+            new MoveToTreeState(this),
+            new MineTreeState(this),
+            new MoveToFire(this),
+            null
+        };
+        NextState();
     }
 
     public RenderLayer RenderLayer => RenderLayer.Avatar;
@@ -29,12 +47,22 @@ internal class Avatar : IGameComponent, ISaveable, IFallable
     {
         canvas.ApplyTransform(Transform);
         canvas.DrawTexture(sprite, 0, 0, 2, 3);
+        if (hasTorch)
+        {
+            canvas.Fill(new Color(92, 78, 46));
+            canvas.StrokeWidth(.25f);
+            canvas.DrawLine(2,2,3,.5f);
+        }
     }
 
     public void Update()
     {
         Vector2 v = GMTK23.Extensions.VectorExtensions.StepTowards(Transform.Position, TargetPos, WalkSpeed * Time.DeltaTime);
         Transform.Position = v;
+        if (AvatarState != null)
+        {
+            AvatarState.Update();
+        }
     }
 
     public void setTargetPos(Vector2 targetPos)
@@ -60,6 +88,7 @@ internal class Avatar : IGameComponent, ISaveable, IFallable
         this.TargetPos = new(Transform.Position.X + 3, Transform.Position.Y + 1);
         this.Transform.Position = new(Transform.Position.X + 3, Transform.Position.Y + 1);
         this.TargetPos = this.Transform.Position;
+        AvatarState = null;
     }
 
     public Rectangle GetBounds()
---
+++ GMTK23/AvatarState.cs
@@ -0,0 +1,68 @@
+﻿using GMTK23;
+
+abstract class AvatarState
+{
+    public Avatar Avatar { get; private set; }
+
+    public AvatarState(Avatar avatar)
+    {
+        this.Avatar = avatar;
+    }
+
+    public abstract void Update();
+}
+
+class MoveToTreeState : AvatarState
+{
+    public MoveToTreeState(Avatar avatar) : base(avatar)
+    {
+    }
+
+    public override void Update()
+    {
+        var tree = Program.World.Find<Tree>();
+        Avatar.TargetPos = new Vector2(tree.transform.Position.X - 1, Avatar.Transform.Position.Y);
+
+        if (Avatar.IsAtTarget)
+        {
+            Avatar.NextState();
+        }
+    }
+}
+
+class MineTreeState : AvatarState
+{
+    public float startTime;
+    public float endTime;
+
+    public MineTreeState(Avatar avatar) : base(avatar)
+    {
+    }
+
+    public override void Update()
+    {
+        if (startTime == 0)
+        {
+            startTime = Time.TotalTime;
+            endTime = startTime + 15;
+        }
+
+        if (endTime != 0
+            && Time.TotalTime > endTime)
+        {
+            Avatar.hasTorch = true;
+            Avatar.NextState();
+        }
+    }
+}
+
+class MoveToFire : AvatarState
+{
+    public MoveToFire(Avatar avatar) : base(avatar) {
+    }
+    public override void Update()
+    {
+        var fire = Program.World.Find<LogFire>();
+        Avatar.TargetPos = new Vector2(fire.transform.Position.X + 4, Avatar.Transform.Position.Y);
+    }
+}
